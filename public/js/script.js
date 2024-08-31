8f235775fd2cd16067ea6d6fbb78fa4efd0c8fec
Kostyantin <podyankee@gmail.com>
2024-08-31 07:36:43 +0300

    websockets public to web

--- public/js/script.js
+++ public/js/script.js
@@ -15,6 +15,12 @@ const fetchBusData = async () => {
 const formatDate = date => date.toISOString().split('T')[0];
 const formatTime = date => date.toTimeString().split(' ')[0].slice(0, 5);
 
+const getTimeRemainingSeconds = departureTime => {
+	const now = new Date();
+	const timeDifference = departureTime - now;
+	return Math.floor(timeDifference / 1000);
+};
+
 const renderBusData = buses => {
 	const tableBody = document.querySelector('#bus tbody');
 	tableBody.textContent = '';
@@ -24,20 +30,57 @@ const renderBusData = buses => {
 
 		nextDepartureDateTimeUTC = new Date(`${bus.nextDeparture.date}T${bus.nextDeparture.time}Z`);
 
+		const remainingSeconds = getTimeRemainingSeconds(nextDepartureDateTimeUTC);
+
+		const remainingTimeText = remainingSeconds < 60 ? 'Отправляется' : bus.nextDeparture.remaining;
+
 		row.innerHTML = `
 		<td>${bus.busNumber}</td>
 		<td>${bus.startPoint} - ${bus.endPoint}</td>
 		<td>${formatDate(nextDepartureDateTimeUTC)}</td>
 		<td>${formatTime(nextDepartureDateTimeUTC)}</td>
+		<td>${remainingTimeText}</td>
 		`;
 
 		tableBody.append(row);
 	});
 };
 
+const initWebSocket = () => {
+	const ws = new WebSocket(`ws://${location.host}`);
+
+	ws.addEventListener('open', () => {
+		console.log('WebSocket connection');
+	});
+
+	ws.addEventListener('message', event => {
+		const buses = JSON.parse(event.data);
+		renderBusData(buses);
+	});
+
+	ws.addEventListener('error', error => {
+		console.log(`WebSocket error: ${error}`);
+	});
+
+	ws.addEventListener('close', () => {
+		console.log('WebSocket connection closed');
+	});
+};
+
+const updateTime = () => {
+	const currentTimeElement = document.getElementById('current-time');
+	const now = new Date();
+	currentTimeElement.textContent = now.toTimeString().split(' ')[0];
+
+	setTimeout(updateTime, 1000);
+};
+
 const init = async () => {
 	const buses = await fetchBusData();
 	renderBusData(buses);
+
+	initWebSocket();
+	updateTime();
 };
 
 init();
